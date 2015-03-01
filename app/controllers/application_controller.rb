846d18108bf8ce833a862f55b71bfd5f9522875e
wildeng <alain.mauri@gmail.com>
2015-03-01 21:47:08 +0100

    minor fixes, introduced error messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def start_connection
-    @mpd = MPD.new APP_CONFIG['remote_server'], APP_CONFIG['remote_port']
-    @mpd.connect
+    begin
+      @mpd = MPD.new APP_CONFIG['remote_server'], APP_CONFIG['remote_port']
+      @mpd.connect
+    rescue Errno::ETIMEDOUT
+      logger.error('Server timeout')
+      flash[:error] = I18n.t('server_timeout')
+    end
   end
 
   def stop_connection
